7198c93d8543dd9d2d9e6833caef7d75d13a5a19
zouliangming <liangmingzou@163.com>
2019-03-04 09:42:49 +0800

    fix #20 problem: Wonderful

---
+++ #20 Valid Parentheses.py
@@ -0,0 +1,55 @@
+'''
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        charDict = {")": "(", "]": "[", "}": "{"}
+        chars = []
+        for c in s:
+            if c in charDict.values():
+                chars.append(c)
+            elif c in charDict.keys():
+                if len(chars) != 0:
+                    if chars[len(chars) - 1] == charDict[c]:
+                        del chars[len(chars) - 1]
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        if len(chars) != 0:
+            return False
+        return True
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isValid("[([]])"))
