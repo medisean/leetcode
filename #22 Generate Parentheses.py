a59fc7d37286fb32ffa46af084759f6df4411bd9
zouliangming <liangmingzou@163.com>
2019-03-08 11:37:57 +0800

    fix #22 problem

--- #22 Generate Parentheses.py
+++ #22 Generate Parentheses.py
@@ -33,7 +33,43 @@ after )
 
 class Solution:
     def generateParenthesis(self, n: int) -> [str]:
-        return []
+        results = ['(']
+        temp = []
+        for i in range(1, 2*n):
+            temp = []
+            for result in results:
+                if result.count("(")  < n and result.count(")")  < n:
+                    temp.append(result + "(")
+                    temp.append(result + ")")
+                elif result.count("(")  < n:
+                    temp.append(result + "(")
+                else:
+                    temp.append(result + ")")
+            results = temp
+        temp = []
+        for result in results:
+            if self.isValid(result):
+                temp.append(result)
+        return temp
+    
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if len(stack) == 0:
+                stack.append(c)
+            elif stack[-1] == "(":
+                if c == "(":
+                    stack.append("(")
+                else:
+                    stack.pop(-1)
+            elif stack[-1] == ")":
+                return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+                
+    
 
 if __name__ == '__main__':
     solution = Solution()
