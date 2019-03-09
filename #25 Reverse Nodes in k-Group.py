08ecf90ffd135c67da92690ed61ca480b2708292
zouliangming <liangmingzou@163.com>
2019-03-09 21:40:48 +0800

    fix #25 problem

--- #25 Reverse Nodes in k-Group.py
+++ #25 Reverse Nodes in k-Group.py
@@ -21,11 +21,35 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+    def PrintSelf(self):
+        p = self
+        while p != None:
+            print(p.val, "-->")
+            p = p.next
 
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if k == 0:
+            return head
+        nodes = []
+        p = head
+        while p != None:
+            nodes.append(p)
+            p = p.next
+        for i in range(int(len(nodes)/k)):
+            for j in range(0, int(k/2)):
+                nodes[i * k + j].val, nodes[(i + 1) * k - j - 1].val = nodes[(i + 1) * k - j - 1].val, nodes[i * k + j].val
         return head
 
 if __name__ == '__main__':
+    l1 = ListNode(1)
+    l2 = ListNode(2)
+    l3 = ListNode(3)
+    l4 = ListNode(4)
+    l5 = ListNode(5)
+    l1.next = l2
+    l2.next = l3
+    l3.next = l4
+    l4.next = l5
     solution = Solution()
-    solution.reverseKGroup(None, 10)
+    solution.reverseKGroup(l1, 2).PrintSelf()
