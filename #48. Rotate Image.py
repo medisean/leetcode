0ab07227481f5ca359ca1a84c4b680a6ae31690c
zouliangming <liangmingzou@163.com>
2019-04-02 20:15:20 +0800

    fix #48 problem

---
+++ #48. Rotate Image.py
@@ -0,0 +1,64 @@
+'''
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+'''
+
+class Solution:
+    def rotate(self, matrix: [[int]]) -> None:
+        results = []
+        k = len(matrix)
+        for i in range(k):
+            result = []
+            for j in range(k):
+                result.append(matrix[k-j-1][i])
+            results.append(result)
+        for i in range(k):
+            for j in range(k):
+                matrix[i][j] = results[i][j]
+        print(matrix)
+
+if __name__ == '__main__':
+    solution = Solution()
+    solution.rotate([
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+])
